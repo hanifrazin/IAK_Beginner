0b981f0ad8f3c14fa873ed6e21ba12b764264b62
“Hanif <hanifrazin@gmail.com>
2018-01-28 14:08:55 +0700

    Membuat Layout Login

--- app/src/main/java/com/example/hanif/iak_beginner/LoginActivity.java
+++ app/src/main/java/com/example/hanif/iak_beginner/LoginActivity.java
@@ -2,12 +2,24 @@ package com.example.hanif.iak_beginner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText et_username;
+    private EditText et_password;
+    private Button btn_cancel;
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        et_username = (EditText)findViewById(R.id.et_username);
+        et_password = (EditText)findViewById(R.id.et_password);
+        btn_cancel = (Button)findViewById(R.id.btn_cancel);
+        btn_login = (Button)findViewById(R.id.btn_login);
     }
 }
